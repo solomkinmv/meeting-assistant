90b3ed284a83415a81e557c5622dc9357ef17dc3
Maksym Solomkin <solomkinmv@gmail.com>
2021-11-11 01:00:06 +0200

    Initialize project structure

---
+++ src/main/java/in/solomk/meeting/assistant/MeetingAssistantApplication.java
@@ -0,0 +1,13 @@
+package in.solomk.meeting.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/solomk/meeting/assistant/MeetingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package in.solomk.meeting.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
