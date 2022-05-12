0d5a89ce21d38836dd30ac51928cefc659ad4eeb
Maksym Solomkin <solomkinmv@gmail.com>
2022-05-13 00:46:18 +0300

    Configure separate build scripts for dev and prod

---
+++ client/webpack.dev.js
@@ -0,0 +1,21 @@
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        historyApiFallback: {
+            rewrites: [
+                {from: /^\/meeting\//, to: '/meeting.html'},
+                {from: /^\/404$/, to: '/404.html'}
+            ]
+        },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080'
+            }
+        }
+    },
+});
