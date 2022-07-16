1e6dd0b965c0d1887feef6f247816d4de5ceb2a8
Maksym Solomkin <solomkinmv@gmail.com>
2022-07-17 00:05:18 +0300

    Add react client skeleton

---
+++ react-client/src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Meeting from "../meeting/Meeting";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Meeting Assistant</h1>
+
+            <Meeting/>
+        </div>
+    );
+}
+
+export default App;
