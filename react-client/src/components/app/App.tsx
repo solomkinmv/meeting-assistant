ad40b083d1b5401de7f18938ac99f12199ccdd42
Maksym Solomkin <solomkinmv@gmail.com>
2022-07-23 00:31:10 +0300

    React router tutorial files

--- react-client/src/components/app/App.tsx
+++ react-client/src/components/app/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import {Link, Outlet} from "react-router-dom";
 import './App.css';
-import Meeting from "../meeting/Meeting";
 
 function App() {
     return (
         <div className="App">
             <h1>Meeting Assistant</h1>
-
-            <Meeting/>
+            <nav
+                style={{
+                    borderBottom: "solid 1px",
+                    paddingBottom: "1rem",
+                }}
+            >
+                <Link to="/meeting">Create meeting</Link> |{" "}
+                <Link to="/404">Not Found</Link> | {" "}
+                <Link to="/invoices">Invoices</Link> |{" "}
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+            <Outlet/>
         </div>
     );
 }
